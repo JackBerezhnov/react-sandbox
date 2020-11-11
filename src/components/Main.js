ae3637d7079aa1050c8def5915faee495fbad24c
Evgeny Berezhnov <33531079+JackBerezhnov@users.noreply.github.com>
2020-11-11 13:58:14 -0800

    create toogle button use state for that

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,23 +4,28 @@ import React, {Component} from 'react';
 class Main extends Component {
     constructor(props) {
         super(props);
-        this.state = {date: new Date()};
+        this.state = {
+            date: new Date(), 
+            isToggleOn: true
+        };
+        this.handleClick = this.handleClick.bind(this);
     }
 
+    handleClick(e) {
+        this.setState(state => ({
+            isToggleOn: !state.isToggleOn
+        }));
+    }
     
     render() {
-
-        function handleClick(e) {
-            e.preventDefault();
-            console.log('This button was clicked');
-        }
-
 
         return (
             <div>
                 <h1>Hello, world!</h1>
                 <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-                <button onClick={handleClick}>Click me!</button>
+                <button onClick={this.handleClick}>
+                    {this.state.isToggleOn ? 'ON' : 'OFF'}
+                </button>
             </div>
         );
     }
