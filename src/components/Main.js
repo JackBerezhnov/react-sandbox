47a3fd0e488875699b7ff80294c26cdf2d4e23d4
Evgeny Berezhnov <33531079+JackBerezhnov@users.noreply.github.com>
2020-11-10 22:28:14 -0800

    Created simple button with handleClick event

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,11 +7,20 @@ class Main extends Component {
         this.state = {date: new Date()};
     }
 
+    
     render() {
+
+        function handleClick(e) {
+            e.preventDefault();
+            console.log('This button was clicked');
+        }
+
+
         return (
             <div>
                 <h1>Hello, world!</h1>
                 <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+                <button onClick={handleClick}>Click me!</button>
             </div>
         );
     }
